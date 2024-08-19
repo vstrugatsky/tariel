aaf9d1885a6150a936a3c6ca876e9c7e81a1348b
Vladimir Strugatsky <v.strugatsky@complianceandrisks.com>
2024-08-18 21:35:38 -0700

    gitignore

--- tests/currencies_test.py
+++ tests/currencies_test.py
@@ -1,5 +1,5 @@
+import model
 from model.currency import Currency
-
 
 def test_format_for_regex():
     # currencies should be sorted by longest first (3 letters as in USD) so that the first separator | is in position 4
